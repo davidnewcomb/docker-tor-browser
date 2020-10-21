399202c6e48a2dac584db758f2047d18ef290a81
David Newcomb <david.newcomb@bigsoft.co.uk>
2020-10-21 16:26:20 +0100

    new preferences

---
+++ browser-cfg/prefs.js
@@ -0,0 +1,9 @@
+// Any comment. You must start the file with a comment!
+
+// user_pref - user may change options
+
+//user_pref("javascript.enabled", false);
+user_pref("app.update.auto", false);
+
+// Tell browser to include http:// in address bar
+user_pref("browser.urlbar.trimURLs", false);
--- startapp.sh
+++ startapp.sh
@@ -3,16 +3,15 @@
 echo "Configuring Tor browser"
 
 cd /browser-cfg
-if [ -f "mozilla.cfg" ]
+if [ -f "prefs.js" ]
 then
-	echo "Copying browser config"
-	cp autoconfig.js /app/Browser/defaults/pref/autoconfig.js
-	cp mozilla.cfg /app/Browser/mozilla.cfg
+	echo "Copying User Preference overrides"
+	cat prefs.js >> /app/Browser/TorBrowser/Data/Browser/profile.default/prefs.js
 fi
 
 echo "Starting Tor browser"
 
 cd /app
-./Browser/start-tor-browser
+./Browser/start-tor-browser --verbose --log /tmp/start-tor-browser.log
 
 echo "Tor browser exited"
