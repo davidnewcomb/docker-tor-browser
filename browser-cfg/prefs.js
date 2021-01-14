32a4e56babc90ab51407521020dcb3a1a9b21425
David Newcomb <david.newcomb@bigsoft.co.uk>
2021-01-14 18:51:38 +0000

    run audio server when running docker

--- browser-cfg/prefs.js
+++ browser-cfg/prefs.js
@@ -7,3 +7,6 @@ user_pref("app.update.auto", false);
 
 // Tell browser to include http:// in address bar
 user_pref("browser.urlbar.trimURLs", false);
+
+// Scroll up/down in bigger steps
+user_pref("mousewheel.default.delta_multiplier_y", 300);
--- run/audio.sh
+++ run/audio.sh
@@ -4,9 +4,10 @@
 #    listening on default port (4713)
 #    allowing anonymous connections
 
-DEBUG="-v"
 DEBUG=""
+DEBUG="-v"
+PA_PID="$1"
 
 echo "Starting Pulse Audio"
-pulseaudio $DEBUG --load="module-native-protocol-tcp auth-anonymous=1" --exit-idle-time=-1
+pulseaudio $DEBUG --load="module-native-protocol-tcp auth-anonymous=1" --exit-idle-time=-1 --use-pid-file=1
 
